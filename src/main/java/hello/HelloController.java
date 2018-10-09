13742aeffa2c1267f77428235c6892b657b27250
javier <javierv31@gmail.com>
2018-10-09 18:59:31 -0500

    Inicio de versionamiento

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	    @RequestMapping("/")
+	    public String index() {
+	        return "Greetings from Spring Boot!";
+	    }
+
+	    @RequestMapping("/load")
+	    public String load() {
+	        return "Greetings from Spring Boot!";
+	    }
+}
---
+++ src/main/java/hello/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author javier
+ *
+ */
+package hello;
