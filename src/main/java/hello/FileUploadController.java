73c657752dad01691bc497037ec585c0d563e4f6
Javier Hernandez <javierv31@gmail.com>
2018-10-16 00:30:38 -0500

    integracion

--- src/main/java/hello/FileUploadController.java
+++ src/main/java/hello/FileUploadController.java
@@ -52,7 +52,7 @@ public class FileUploadController {
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
-    @PostMapping("/")
+    @PostMapping("/testLoad")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
             RedirectAttributes redirectAttributes) {
 
